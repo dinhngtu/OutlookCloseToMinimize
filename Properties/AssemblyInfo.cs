ba8fbf319aebf6fb3db4a95d77db5340682eb8e8
Tu Dinh <dinhngtu@users.noreply.github.com>
2022-10-15 21:48:39 +0200

    1.4.2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,9 +9,9 @@ using System.Security;
 [assembly: AssemblyTitle("OutlookCloseToMinimize")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Tu Dinh")]
 [assembly: AssemblyProduct("OutlookCloseToMinimize")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © Tu Dinh 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.4.2.0")]
+[assembly: AssemblyFileVersion("1.4.2.0")]
 
